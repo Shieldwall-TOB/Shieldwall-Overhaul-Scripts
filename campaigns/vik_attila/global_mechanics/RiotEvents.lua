652e4e7d29430a025ccb407df11fdadbf96b9771
DrunkFlamingo <38512127+DrunkFlamingo@users.noreply.github.com>
2019-12-25 19:46:32 -0500

    added more riot events

--- campaigns/vik_attila/global_mechanics/RiotEvents.lua
+++ campaigns/vik_attila/global_mechanics/RiotEvents.lua
@@ -12,6 +12,20 @@ local riot_events = {
             end
         end,
         is_dilemma = true
+    },
+    {
+        name = "shield_rebellion_stoning_",
+        condition = function(riot_manager) --:RIOT_MANAGER
+            local region = dev.get_region(riot_manager.key)
+            return region:has_governor() and cm:random_number(100) > 33 and not Check.does_char_have_household_guard(region:governor())
+        end,
+        response = function(context) --:WHATEVER
+            local region_key = string.gsub(context:dilemma(), "shield_rebellion_stoning_", "")
+            local region = dev.get_region(region_key)
+            local gov = region:governor():command_queue_index()
+            cm:kill_character("character_cqi:"..tostring(gov), false, true)
+        end,
+        is_dilemma = false
     }
 }--:vector<{name: string, condition: (function(rioting_region: RIOT_MANAGER) --> boolean), response: (function(context: WHATEVER)), is_dilemma: boolean}>
 
