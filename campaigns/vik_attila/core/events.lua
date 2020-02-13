ca3c89c6836ee5b9de574fb99c33c989df4364e2
DrunkFlamingo <38512127+DrunkFlamingo@users.noreply.github.com>
2020-02-13 12:47:48 -0500

    added turnstart dilemma

--- campaigns/vik_attila/core/events.lua
+++ campaigns/vik_attila/core/events.lua
@@ -2,13 +2,14 @@
 --used to coordinate the trigger of dilemmas.
 local game_events = {}
 game_events.turnstart_events = {} --:map<int, vector<string>>
+game_events.turnstart_queue = {} --:vector<{faction: string, dilemma: string}>
 game_events.postbattle_events = {} --:map<int, vector<string>>
 game_events.post_occupy_or_sack_events = {} --:map<int, vector<string>>
 game_events.last_event_turn = {} --:map<string, int>
 game_events.event_conditions = {} --:map<string, (function(context: WHATEVER) --> boolean)>
 game_events.save = {
     name = "game_event_manager",
-    for_save = {"last_event_turn"}
+    for_save = {"last_event_turn", "turnstart_queue"}
 }
 
 local max_priority_levels = 4 --:int
@@ -33,9 +34,15 @@ dev.first_tick(function(context)
             return context:faction():is_human() and ((game_events.last_event_turn[context:faction():name()] or 0) < cm:model():turn_number())
         end,
         function(context)
+            for i = 1, #game_events.turnstart_queue do
+                cm:trigger_dilemma(game_events.turnstart_queue[i].faction, game_events.turnstart_queue[i].dilemma, true)
+                table.remove(game_events.turnstart_queue, i)
+                return
+            end
+            local turn_difference = cm:model():turn_number() - (game_events.last_event_turn[context:faction():name()] or 0)
             for i = 1, max_priority_levels do
                 local events = game_events.turnstart_events[i]
-                if events then 
+                if events and turn_difference >= i then 
                     for j = 1, #events do
                         local event_key = events[j]
                         if game_events.event_conditions[event_key] then
@@ -86,6 +93,16 @@ local function add_turnstart_event(event_key, condition, priority)
     game_events.event_conditions[event_key] = condition
 end
 
+--v [NO_CHECK] function(event_key: string, faction_name: string, above_all_others: boolean?)
+local function queue_event_for_turn_start(event_key, faction_name, above_all_others)
+    if above_all_others then
+        table.insert(game_events.turnstart_queue, {faction = faction_name, dilemma = event_key}, 1)
+    else
+        table.insert(game_events.turnstart_queue, {faction = faction_name, dilemma = event_key})
+    end
+end
+
 return {
+    trigger_turnstart_dilemma = queue_event_for_turn_start,
     add_turnstart_event = add_turnstart_event
 }
