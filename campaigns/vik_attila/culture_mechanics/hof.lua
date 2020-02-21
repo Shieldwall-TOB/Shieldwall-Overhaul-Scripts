560d1c38e1ec39d600d3c2c88afb655d01ddd3d6
DrunkFlamingo-Laptop <38512127+DrunkFlamingo@users.noreply.github.com>
2020-02-21 18:28:33 -0500

    fixed details in hof script, incl cd

--- campaigns/vik_attila/culture_mechanics/hof.lua
+++ campaigns/vik_attila/culture_mechanics/hof.lua
@@ -1,5 +1,6 @@
 local hof_key = ""
 local hof_dilemma = ""
+local sc_key = "vik_sub_cult_viking_gael"
 
 local cooldowns = {
     [1] = 40,
@@ -17,6 +18,17 @@ local function get_cooldown(hof_count)
     return cooldowns[hof_count] or 10
 end
 
+--v function(region_list: CA_REGION_LIST) --> int
+local function count_hofs(region_list)
+    local count = 0
+    for i = 0, region_list:num_items() - 1 do
+        if region_list:item_at(i):building_exists(hof_key) then
+            count = count + 1;
+        end
+    end
+    return count
+end
+
 local hofs = {
 
 }--:map<string, number>
@@ -24,29 +36,30 @@ local hofs = {
 dev.first_tick(function(context) 
     if dev.is_new_game() then
         local players = cm:get_human_factions()
-        for i = 1, #players do hofs[players[i]] = 0 end
+        for i = 1, #players do hofs[players[i]] = 1 end
     end
     dev.eh:add_listener(
         "Hofs",
         "FactionTurnStart",
         function(context)
-            return context:faction():is_human() and context:faction():faction_leader():has_trait("shield_heathen_pagan")
+            return context:faction():is_human() and context:faction():subculture() == sc_key
         end,
         function(context)
-            local count = 0
+          
             local faction = context:faction()
             local region_list = faction:region_list()
-            for i = 0, region_list:num_items() - 1 do
-                if region_list:item_at(i):building_exists(hof_key) then
-                    count = count + 1;
-                end
+            if not context:faction():faction_leader():has_trait("shield_heathen_pagan") then
+                cm:add_restricted_building_level_record_for_faction(faction:name(), hof_key)
+                hofs[faction:name()] = 999
+                return
             end
+            local count = count_hofs(region_list)
             if count == 0 then
                 return
             end
             local cooldown_current = hofs[faction:name()] or 0
             local target = get_cooldown(count)
-            if cooldown_current > target then hofs[faction:name()] = (target - 3) else
+            if cooldown_current > target then hofs[faction:name()] = (target - 3) elseif hofs[faction:name()] > 0 then
                 hofs[faction:name()] = cooldown_current - 1
             end
         end,
@@ -54,9 +67,13 @@ dev.first_tick(function(context)
     )
 
     dev.Events.add_turnstart_event(hof_dilemma, function(context)
-        local key = context:faction():name()
-        return hofs[key] and hofs[key] == 0 
-    end, 4, false)
+        local faction = context:faction()
+        return hofs[faction:name()] == 0 
+    end, 4, false, function(context)
+        local faction = context:faction()
+        local cd = get_cooldown(count_hofs(faction:region_list()))
+        hofs[faction:name()] = cd 
+    end)
 end)
 
 
