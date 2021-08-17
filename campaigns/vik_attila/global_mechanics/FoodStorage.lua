973bf7ad21b2de6aeb70ddcef3fb1c3c45c3e214
DrunkFlamingo <38512127+DrunkFlamingo@users.noreply.github.com>
2021-08-17 10:58:50 -0400

    food storage in startpos

--- campaigns/vik_attila/global_mechanics/FoodStorage.lua
+++ campaigns/vik_attila/global_mechanics/FoodStorage.lua
@@ -17,3 +17,16 @@ local fs = PettyKingdoms.FoodStorage
 for building, quantity in pairs(building_storage_effects) do
     fs.add_food_storage_effect_to_building(building, quantity)
 end
+
+local faction_food_storage_startpos = {
+    ["vik_fact_west_seaxe"] = 86
+}--:map<string, number>
+
+
+dev.new_game(function(context)
+    local humans = cm:get_human_factions()
+    for i = 1, #humans do
+        local fsm = PettyKingdoms.FoodStorage.get(humans[i])
+        fsm:set_food_in_storage(faction_food_storage_startpos[humans[i]] or 0) 
+    end
+end)
